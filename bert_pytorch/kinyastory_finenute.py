c6728ac5fc7dacb4dafc27e03e2b5a52bbd4dec3
kclaudeeager <claudekwizera003@gmail.com>
2024-04-25 23:53:36 +0200

    Hangling test dataset

--- bert_pytorch/kinyastory_finenute.py
+++ bert_pytorch/kinyastory_finenute.py
@@ -22,9 +22,7 @@ class KinyaStoryFinetune:
 
     
     def finetune(self, train_dataset, output_path, last_saved_epoch=None, batch_size=64,test_dataset=None, epochs=10, num_workers=5, with_cuda=True, log_freq=10, corpus_lines=None, cuda_devices=None, on_memory=True, lr=1e-3, adam_weight_decay=0.01, adam_beta1=0.9, adam_beta2=0.999):
-        train_data_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers)
-        test_data_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers) if test_dataset is not None else None
-        
+       
         '''
         __init__(self, bert: BERT, vocab_size: int,
                  train_dataloader: DataLoader, test_dataloader: DataLoader = None,
@@ -32,6 +30,9 @@ class KinyaStoryFinetune:
                  with_cuda: bool = True, cuda_devices=None, log_freq: int = 10, wandb_project_name="project-ablations",wandb_name="kinya-bert-training_new",wandb_reinit=True,model_file_path="output/bert.model",last_saved_epoch=None):
         '''
 
+        train_data_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers)
+        test_data_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers) if test_dataset is not None else None
+        
         trainer = KinyaStoryBERTTrainer(bert=self.model, vocab_size=len(self.vocab), train_dataloader=train_data_loader, test_dataloader=test_data_loader, lr=lr, betas=(adam_beta1, adam_beta2), weight_decay=adam_weight_decay, with_cuda=with_cuda, cuda_devices=cuda_devices, log_freq=log_freq, fine_tuned_file_path=output_path, last_saved_epoch=last_saved_epoch,wandb_project_name="project-ablations", wandb_name="kinya-bert-finetuning", wandb_reinit=True)
         
         
@@ -89,11 +90,12 @@ def main():
     
     kinya_story_finetune = KinyaStoryFinetune(bert, tokenizer, device=args.device)
     train_dataset = KinyaStoryNewDataset(args.train_dataset, vocab, seq_len=args.seq_len, on_memory=True)
+    test_dataset = KinyaStoryNewDataset(args.test_dataset, vocab, seq_len=args.seq_len, on_memory=True) if args.test_dataset is not None else None
 
     if not os.path.exists(args.output_path):
         os.makedirs(args.output_path)
 
-    kinya_story_finetune.finetune(train_dataset, args.output_path, last_saved_epoch=args.last_saved_epoch, batch_size=64,test_dataset=args.test_dataset, epochs=args.epochs, num_workers=5, with_cuda=True, log_freq=10, corpus_lines=None, cuda_devices=None, on_memory=True, lr=1e-3, adam_weight_decay=0.01, adam_beta1=0.9, adam_beta2=0.999)
+    kinya_story_finetune.finetune(train_dataset, args.output_path, last_saved_epoch=args.last_saved_epoch, batch_size=64,test_dataset=test_dataset, epochs=args.epochs, num_workers=5, with_cuda=True, log_freq=10, corpus_lines=None, cuda_devices=None, on_memory=True, lr=1e-3, adam_weight_decay=0.01, adam_beta1=0.9, adam_beta2=0.999)
 
     print("Finetuning complete")
 
