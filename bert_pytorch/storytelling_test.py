ffef81ca8e4dfbae8d3c0c00b281ff3fe44e4542
kclaudeeager <claudekwizera003@gmail.com>
2024-04-26 23:34:42 +0200

    Make it better

--- bert_pytorch/storytelling_test.py
+++ bert_pytorch/storytelling_test.py
@@ -52,89 +52,120 @@ class BERTInference:
 
     
     def generate_text(self, starting_text, max_length=128):
+        #try:
         if starting_text is None or len(starting_text) == 0:
             print("Starting text is empty")
             return None
-    
+        # Create a template file and insert the starting text
         starting_text_temp_file = 'kinyastory_data/starting_text_temp.txt'
         with open(starting_text_temp_file, 'w') as f:
             f.write(starting_text+'\n')
-    
+
         inference_dataset = KinyaStoryNewDataset(corpus_path=starting_text_temp_file, vocab=self.vocab, seq_len=128,is_inference=True)
         inference_loader = torch.utils.data.DataLoader(inference_dataset, batch_size=4, shuffle=False)
+        data_iter = tqdm.tqdm(enumerate(inference_loader),
+                              desc=" Inferencing",
+                              total=len(inference_loader),
+                              bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]")
     
         concatinated_text = ""
-        for batch in inference_loader:
-            generated = batch['bert_input'].to(self.device)
-            segment_label = batch['segment_label'].to(self.device)
-            label = batch['bert_label'].to(self.device)
-            is_next = batch['is_next'].to(self.device)
-    
+        avg_loss = 0.0
+        total_correct = 0
+        total_element = 0
+        for i, data in data_iter:
+            data = {key: value.to(self.device) for key, value in data.items()}
+            generated = data['bert_input']
+            segment_label = data['segment_label']
+            label = data['bert_label']
+            is_next = data['is_next']
+            
+            # Move the generated tensor to the device
+            generated = generated.to(self.device)
+            segment_label = segment_label.to(self.device)
+            label = label.to(self.device)
+            is_next = is_next.to(self.device)
+            # delete the starting text temp file
             os.remove(starting_text_temp_file)
     
+            print("Setting model to eval mode...")
             self.model.eval()
     
             with torch.no_grad():
-                print("The generated text is: ", decode(self.tokenizer, generated.squeeze().tolist()))
-    
-                predictions = self.model.forward(generated, segment_label)
-    
-                predictions_masked = predictions[0].unsqueeze(0)
-                predictions_next = predictions[1]
-    
+
+                # Observe the generated text
+
+                #print("The generated text is: ", decode(self.tokenizer, generated.squeeze().tolist()))
+                
+                predictions_next, predictions_masked = self.model.forward(generated, segment_label)
+               
+        
                 label_loss = calculate_nll(predictions_masked.transpose(1, 2), label)
                 label_perplexity = calculate_perplexity(label_loss)
                 print(f"Label loss: {label_loss}, Label perplexity: {label_perplexity}")
-    
+
                 next_loss = calculate_nll(predictions_next, is_next.squeeze())
                 next_perplexity = calculate_perplexity(next_loss)
                 print(f"Next loss: {next_loss}, Next perplexity: {next_perplexity}")
-    
+
                 total_loss = label_loss + next_loss
                 total_perplexity = calculate_perplexity(total_loss)
                 print(f"Total loss: {total_loss}, Total perplexity: {total_perplexity}")
-    
-                predictions_masked = predictions[0].squeeze(0).to(self.device)
-                predictions_next = predictions[1].squeeze(0).to(self.device)
-    
+               
+                predictions_masked =predictions_masked.squeeze(0).to(self.device)
+                predictions_next = predictions_next.squeeze(0).to(self.device)
+                
+                # Replace the masked tokens in the input with the predicted masked tokens
                 masked_indices = (generated == self.vocab["[MASK]"]).nonzero(as_tuple=True)[1]
-    
+
+                # Replace the masked tokens in the input with the predicted masked tokens
+                masked_indices = (generated == self.vocab["[MASK]"]).nonzero(as_tuple=True)[1]
+                
                 for idx in masked_indices:
                     next_masked = torch.argmax(predictions_masked[idx, :], dim=-1)
                     generated[0, idx] = next_masked
-    
+                
+                
+                # Append the predicted next word token to the input
                 next_word = torch.argmax(predictions_next[-1, :], dim=-1).unsqueeze(0)
-    
+
+                # Early stopping condition
                 if next_word.item() == self.vocab['[PAD]'] or generated.size(1) > max_length:
                     print(f"Stopping generation at padding token {next_word.item()}")
                     return decode(self.tokenizer, generated.squeeze().tolist())
-    
+                    
+
+
                 generated = torch.cat((generated, next_word.unsqueeze(0)), dim=1)
-    
-                print("Decoding generated text...")
-                decoded_text = decode(self.tokenizer, generated.squeeze().tolist())
-    
-                print("Calculating BLEU and ROUGE scores...")
-                bleu_score = calculate_bleu(starting_text, decoded_text)
-                rouge_score = calculate_rouge(starting_text, decoded_text)
-                print(f"BLEU score: {bleu_score}, ROUGE score: {rouge_score}")
-    
-                wandb_logs = {
-                    "Label Loss": label_loss,
-                    "Label Perplexity": label_perplexity,
-                    "Next Loss": next_loss,
-                    "Next Perplexity": next_perplexity,
-                    "Total Loss": total_loss,
-                    "Total Perplexity": total_perplexity,
-                    "BLEU": bleu_score, 
-                    "ROUGE": rouge_score,
+
+                # If the length of generated exceeds max_length, remove the first token
+                # if generated.size(1) > max_length:
+                #     generated = generated[:, 1:]
+                
+            print("Decoding generated text...")
+            decoded_text = decode(self.tokenizer, generated.squeeze().tolist())
+
+            # Claculate the Rouge and Blue
+            print("Calculating BLEU and ROUGE scores...")
+            bleu_score = calculate_bleu(starting_text, decoded_text)
+            rouge_score = calculate_rouge(starting_text, decoded_text)
+            print(f"BLEU score: {bleu_score}, ROUGE score: {rouge_score}")
+            wandb_logs = {
+                "Label Loss": label_loss,
+                "Label Perplexity": label_perplexity,
+                "Next Loss": next_loss,
+                "Next Perplexity": next_perplexity,
+                "Total Loss": total_loss,
+                "Total Perplexity": total_perplexity,
+                "BLEU": bleu_score, 
+                "ROUGE": rouge_score,
                 }
-                wandb.log(wandb_logs)
-    
-                concatinated_text += decoded_text
-                print(f"Generated text: {decoded_text}")
-    
+            wandb.log(wandb_logs)
+            concatinated_text += decoded_text
+            print(f"Generated text: {decoded_text}")
         return self.generate_text(concatinated_text, max_length)
+        # except Exception as e:
+        #     print(f"Error generating text: {e}")
+        # return None
 
      
         
